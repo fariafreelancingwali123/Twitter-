72b9c7790ad4aa6718f3d5c53617cbf8ca3fe618
fariafreelancingwali123 <fariasammer@gmail.com>
2025-05-02 22:18:45 +0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+include('db.php');
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $conn->real_escape_string($_POST['username']);
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE username = '$username'");
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            header("Location: index.php");
+            exit;
+        } else {
+            echo "Invalid password.";
+        }
+    } else {
+        echo "No such user.";
+    }
+}
+?>
+
+<form method="POST">
+    <h2>Login</h2>
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <button type="submit">Login</button>
+</form>
